f8ece1c135cd4404a5545c936af1d609ef5bb6df
Sutiksh Verma <verma.sutiksh27@gmail.com>
2019-08-28 14:33:09 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:event_manager/pages/home.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Event Manager",
+    home: Home(),
+  ));
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          "Event Manager",
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 2.0,
+      ),
+    );
+  }
+}
